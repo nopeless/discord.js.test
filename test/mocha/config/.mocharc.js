b41761745c4d18f0e2a65b5d7719d4d3c1ca34a9
nopeless <38830903+nopeless@users.noreply.github.com>
2021-03-03 01:48:11 +0900

    docs: mocharc example

---
+++ test/mocha/config/.mocharc.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  // a response should happen under 1 second
+  slow: 2000,
+  // set timeout to 1 hour, which is extremely long. edit this as necessary
+  timeout: 60 * 60 * 1000,
+};
