8d444f63ac777682d001ba602ea4152c699932ec
nopeless <38830903+nopeless@users.noreply.github.com>
2021-03-03 22:04:45 +0900

    refactor: delete automerge as I will manually track tags

--- automerge.sh
+++
@@ -1,7 +0,0 @@
-set -e
-# set current head to master
-git checkout master
-# pull the discord.js master
-git pull repo-master
-# automerge if possible
-git merge repo-master --ff-only --no-edit -m "Automerge.sh - auto merged via update"
